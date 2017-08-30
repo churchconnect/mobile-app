06f0eafeccd9d5dc941f4e215182529b8e30a651
Ryan Heathcote <rheathcote@sharptop.co>
2017-08-30 14:04:29 -0400

    Remove the copyright headers now that we are on AGPL

--- www/src/services/f7.js
+++ www/src/services/f7.js
@@ -1,10 +1,3 @@
-/*
- * Copyright (c) 2016 by SharpTop Software, LLC
- * All rights reserved. No part of this software project may be used, reproduced, distributed, or transmitted in any
- * form or by any means, including photocopying, recording, or other electronic or mechanical methods, without the prior
- * written permission of SharpTop Software, LLC. For permission requests, write to the author at info@sharptop.co.
- */
-
 import 'framework7'
 
 export const F7 = new Framework7({
